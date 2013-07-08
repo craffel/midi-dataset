550cd79f14c827e3ddcb634dbe5f4c35d5317ab4
Colin Raffel <craffel@gmail.com>
2013-07-08 13:37:17 -0400

    Adding velocity, docs, skeleton of onsets_to_strength function

--- midi_to_features.py
+++ midi_to_features.py
@@ -11,11 +11,21 @@ import string
 
 # <codecell>
 
-# Write out the unique time of note-ons
-def get_onsets( MIDIFile ):
+def get_onsets( MIDIData ):
+    '''
+    Given a midi.Pattern, extract onset locations and their velocities
+    
+    Input:
+        MIDIData - midi.Pattern object, via midi.read_midifile( MIDIFile )
+    Output:
+        onsets - onset times, in seconds
+        velocities - velocity of each onset time
+    '''
     MIDIData = midi.read_midifile( MIDIFile )
     # Array for holding onset locations (in seconds)
     onsets = np.array([])
+    # Array for velocities too
+    velocities = np.array([])
     # Scale for MIDI tick numbers, to convert to seconds
     tickScale = 1
     foundTempo = 0
@@ -46,6 +56,28 @@ def get_onsets( MIDIFile ):
                     # Check for duplicates before adding in
                     if not (onsets == time).any():
                         onsets = np.append( onsets, time )
+                        velocities = np.append( velocities, event.velocity )
     # Return onset array
-    return onsets
+    return onsets, velocities
+
+# <codecell>
+
+def onsets_to_strength( onsets, velocities ):
+    '''
+    Given onset locations and their velocities, create an onset strength stream
+    
+    Input:
+        onsets - onset locations, in seconds
+        velocities - velocity of each onset
+    Output:
+        onset_strength - onset strength function
+        fs - sampling rate of the onset strength function
+    '''
+    # Define a sampling rate for the signal
+    fs = 1000
+    # Create an empty signal
+    onset_strength = np.zeros( fs*onsets.max() )
+    # Fill in onset values
+    
+    return onset_strength, fs
 
