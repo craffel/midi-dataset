Fixed bugs in MRR computation

experiments/hashing_parameter_search.py
@@ -95,43 +95,10 @@ def statistics(X, Y):
 
 # <codecell>
 
-def fast_binary_distance(X, Y):
-    '''
-    Compute the binary (matching) distance between all columns of X and all columns of Y
-    
-    Input:
-        X - M x N binary matrix
-        Y - M x K binary matrix
-    Output:
-        D - N x K distance matrix such that D[i, j] = sum(X[:, i] != Y[:, j])
-    '''
-    
-    (M, N) = X.shape
-    assert Y.shape[0] == M
-    (M, K) = Y.shape
-    assert X.dtype == np.bool
-    assert Y.dtype == np.bool
-
-    D = np.zeros((N, K), dtype=np.int)
-    
-    weaver = r"""
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < K; j++) {
-            for (int k = 0; k < M; k++) {
-                D[i*N + j] += X[k*N + i]^Y[k*N + j];
-            }
-        }
-    }
-"""
-    scipy.weave.inline(weaver, arg_names=['M', 'N', 'K', 'X', 'Y', 'D'])
-    return D
-
-# <codecell>
-
 def mean_reciprocal_rank(X, Y):
     ''' Computes the mean reciprocal rank of the correct codeword '''
     # Compute distances between each codeword and each other codeword
-    distance_matrix = fast_binary_distance(X, Y)
+    distance_matrix = scipy.spatial.distance.cdist(X.T, Y.T, metric='hamming')
     # Rank is the number of distances smaller than the correct distance, which is the value on the diagonal
     return np.mean(1./(distance_matrix.T <= np.diag(distance_matrix)).sum(axis=0))
 
@@ -156,7 +123,7 @@ def parameter_space_2():
     # Possible values for each hyperparameter to take
     hp_values = collections.OrderedDict()
     hp_values['n_bits'] = np.random.choice([16, 24])
-    hp_values['n_layers'] = np.random.choice([3])
+    hp_values['n_layers'] = 3
     hp_values['alpha_XY'] = np.random.choice(np.array(np.linspace(0, 2, 201), dtype=theano.config.floatX))
     hp_values['m_XY'] = np.random.choice(17)
     hp_values['alpha_X'] = np.random.choice(np.array(np.linspace(0, 2, 201), dtype=theano.config.floatX))
@@ -197,7 +164,7 @@ patience_increase = 1.2
 # Maximum number of batches to train on
 max_iter = 200*epoch_size
 # Use this many samples to compute mean reciprocal rank
-n_mrr_samples = 1000
+n_mrr_samples = 250
 
 # Set up paths
 base_data_directory = '../data'
@@ -302,9 +269,6 @@ while True:
                 epoch_result[name + '_hash_entropy_X'] = hash_entropy(X_output > 0)
                 epoch_result[name + '_hash_entropy_Y'] = hash_entropy(Y_output > 0)
                 mrr_samples = np.random.choice(N, n_mrr_samples, False)
-                epoch_result[name + '_mean_reciprocal_rank'] = mean_reciprocal_rank(X_output[:, mrr_samples] > 0,
-                                                                                    Y_output[:, mrr_samples] > 0)
-
             if epoch_result['validate_cost'] < current_validate_cost:
                 if epoch_result['validate_cost'] < improvement_threshold*current_validate_cost:
                     patience *= patience_increase
@@ -313,7 +277,10 @@ while True:
                                                                                      improvement_threshold,
                                                                                      current_validate_cost)
                 current_validate_cost = epoch_result['validate_cost']
-                    
+            # Only compute MRR on validate
+            epoch_result[name + '_mean_reciprocal_rank'] = mean_reciprocal_rank(X_output[:, mrr_samples] > 0,
+                                                                                Y_output > 0)
+
             epoch_results.append(epoch_result)
             print '    patience : {}'.format(patience)
             print '    current_validation_cost : {}'.format(current_validate_cost)