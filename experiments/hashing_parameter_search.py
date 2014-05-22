91b817b693c7242ca3753c55850b8b38c66390d3
Colin Raffel <craffel@gmail.com>
2014-05-22 14:45:27 -0400

    A more carefully tuned parameter space

--- experiments/hashing_parameter_search.py
+++ experiments/hashing_parameter_search.py
@@ -46,6 +46,26 @@ def parameter_space_2():
 
 # <codecell>
 
+def parameter_space_3():
+    hp_values = collections.OrderedDict()
+    # 24 bits works better than 16
+    hp_values['n_bits'] = 24
+    hp_values['n_layers'] = 3
+    # Total alpha should be between 3 and 6
+    total_alpha = 3*np.random.random_sample() + 3
+    # Compute random proportion of each alpha
+    alpha_proportions = np.random.random_sample(3)
+    alpha_proportions /= alpha_proportions.sum()
+    hp_values['alpha_XY'] = np.array([total_alpha*alpha_proportions[0]], dtype=theano.config.floatX)[0]
+    hp_values['m_XY'] = np.random.choice(10)
+    hp_values['alpha_X'] = np.array([total_alpha*alpha_proportions[1]], dtype=theano.config.floatX)[0]
+    hp_values['m_X'] = np.random.choice(10)
+    hp_values['alpha_Y'] = np.array([total_alpha*alpha_proportions[2]], dtype=theano.config.floatX)[0]
+    hp_values['m_Y'] = np.random.choice(10)
+    return hp_values
+
+# <codecell>
+
 # First neural net, for chroma vectors
 X_p_input = T.matrix('X_p_input')
 X_n_input = T.matrix('X_n_input')
@@ -80,7 +100,7 @@ n_mrr_samples = 500
 
 # Set up paths
 base_data_directory = '../data'
-result_directory = os.path.join(base_data_directory, 'parameter_search_2')
+result_directory = os.path.join(base_data_directory, 'parameter_search_3')
 training_data_directory = os.path.join(base_data_directory, 'hash_dataset')
 if not os.path.exists(result_directory):
     os.makedirs(result_directory)
@@ -99,7 +119,7 @@ Y_validate_n = Y_validate[:, np.random.permutation(Y_validate.shape[1])]
 
 while True:
     # Randomly choose a value for each hyperparameter
-    hp = parameter_space_2()
+    hp = parameter_space_3()
     # A list of results dicts, one per epoch
     epoch_results = []
     # Make a subdirectory for this parameter setting
