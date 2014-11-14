ed7a426d50fd2f9056671692c1e8a79f982728ce
Colin Raffel <craffel@gmail.com>
2014-11-14 12:35:21 -0500

    Using new training loop function in cross_modality_hashing

--- experiments/hashing_parameter_search.py
+++ experiments/hashing_parameter_search.py
@@ -66,38 +66,6 @@ def parameter_space_3():
 
 # <codecell>
 
-# First neural net, for chroma vectors
-X_p_input = T.matrix('X_p_input')
-X_n_input = T.matrix('X_n_input')
-# Second neural net, for MIDI piano roll
-Y_p_input = T.matrix('Y_p_input')
-Y_n_input = T.matrix('Y_n_input')
-# Symbolic hyperparameters
-alpha_XY = T.scalar('alpha_XY')
-m_XY = T.scalar('m_XY')
-alpha_X = T.scalar('alpha_X')
-m_X = T.scalar('m_X')
-alpha_Y = T.scalar('alpha_Y')
-m_Y = T.scalar('m_Y')
-# SGD learning rate
-learning_rate = 1e-4
-# SGD momentum
-momentum = .9
-# Mini-batch size
-batch_size = 10
-# Number of mini-batches per epoch
-epoch_size = 1000
-# Always train on at least this many batches
-initial_patience = 10000
-# Validation cost must decrease by this factor to increase patience
-improvement_threshold = 0.99
-# Amount to increase patience when validation cost has decreased
-patience_increase = 1.2
-# Maximum number of batches to train on
-max_iter = 200*epoch_size
-# Use this many samples to compute mean reciprocal rank
-n_mrr_samples = 500
-
 # Set up paths
 base_data_directory = '../data'
 result_directory = os.path.join(base_data_directory, 'parameter_search_3')
@@ -110,18 +78,18 @@ X, Y = hashing_utils.load_data(training_data_directory)
 # Split into train and validate and standardize
 X_train, Y_train, X_validate, Y_validate = hashing_utils.train_validate_split(X, Y)
 
+# Use this many samples to compute mean reciprocal rank
+n_mrr_samples = 500
 # Pre-compute indices over which to compute mrr
 mrr_samples = np.random.choice(X_validate.shape[1], n_mrr_samples, False)
 
-# Create fixed negative example validation set
-X_validate_n = X_validate[:, np.random.permutation(X_validate.shape[1])]
-Y_validate_n = Y_validate[:, np.random.permutation(Y_validate.shape[1])]
+# Compute layer sizes.  Middle layers are nextpow2(input size)
+hidden_layer_size_X = int(2**np.ceil(np.log2(X_train.shape[0])))
+hidden_layer_size_Y = int(2**np.ceil(np.log2(Y_train.shape[0])))
 
 while True:
     # Randomly choose a value for each hyperparameter
     hp = parameter_space_3()
-    # A list of results dicts, one per epoch
-    epoch_results = []
     # Make a subdirectory for this parameter setting
     parameter_string = ','.join(["{}={}".format(k, round(v, 2)) for (k, v) in hp.items()])
     print
@@ -134,93 +102,18 @@ while True:
     if os.path.exists(trial_directory):
         continue
     os.makedirs(trial_directory)
-    # Save the parameter dict
-    with open(os.path.join(trial_directory, 'parameters.pkl'), 'wb') as f:
+    # Save the hyperparameter dict
+    with open(os.path.join(trial_directory, 'hyperparameters.pkl'), 'wb') as f:
         pickle.dump(hp, f)
     
-    # Compute layer sizes.  Middle layers are nextpow2(input size)
-    hidden_layer_size_X = int(2**np.ceil(np.log2(X_train.shape[0])))
-    layer_sizes_x = [X_train.shape[0]] + [hidden_layer_size_X]*(hp['n_layers'] - 1) + [hp['n_bits']]
-    hidden_layer_size_Y = int(2**np.ceil(np.log2(X_train.shape[0])))
-    layer_sizes_y = [Y_train.shape[0]] + [hidden_layer_size_Y]*(hp['n_layers'] - 1) + [hp['n_bits']]
-    hasher = cross_modality_hashing.SiameseNet(layer_sizes_x, layer_sizes_y)
-    
-    # Create theano symbolic function for cost
-    hasher_cost = hasher.cross_modality_cost(X_p_input, X_n_input, Y_p_input, Y_n_input,
-                                             alpha_XY, m_XY, alpha_X, m_X, alpha_Y, m_Y)
-    # Function for optimizing the neural net parameters, by minimizing cost
-    train = theano.function([X_p_input, X_n_input, Y_p_input, Y_n_input,
-                             alpha_XY, m_XY, alpha_X, m_X, alpha_Y, m_Y],
-                            hasher_cost,
-                            updates=cross_modality_hashing.gradient_updates_momentum(hasher_cost,
-                                                                                     hasher.params,
-                                                                                     learning_rate,
-                                                                                     momentum))
-    # Compute cost without trianing
-    cost = theano.function([X_p_input, X_n_input, Y_p_input, Y_n_input,
-                             alpha_XY, m_XY, alpha_X, m_X, alpha_Y, m_Y], hasher_cost)
+    epochs, parameters = cross_modality_hashing.train_cross_modality_hasher(X_train, Y_train, X_validate, Y_validate, 
+                             [hidden_layer_size_X]*(hp['n_layers'] - 1),
+                             [hidden_layer_size_Y]*(hp['n_layers'] - 1),
+                             hp['alpha_XY'], hp['m_XY'], hp['alpha_X'], hp['m_X'], hp['alpha_Y'],
+                             hp['m_Y'], hp['n_bits'], mrr_samples=mrr_samples)
 
-    # Keep track of the patience - we will always increase the patience once
-    patience = initial_patience/patience_increase 
-    current_validate_cost = np.inf
-    
-    # Functions for computing the neural net output on the train and validation sets
-    X_train_output = hasher.X_net.output(X_train)
-    Y_train_output = hasher.Y_net.output(Y_train)
-    X_validate_output = hasher.X_net.output(X_validate)
-    Y_validate_output = hasher.Y_net.output(Y_validate)
-    
-    for n, (X_p, Y_p, X_n, Y_n) in enumerate(hashing_utils.get_next_batch(X_train, Y_train, batch_size, max_iter)):
-        train_cost = train(X_p, X_n, Y_p, Y_n, hp['alpha_XY'], hp['m_XY'], hp['alpha_X'], hp['m_X'], hp['alpha_Y'], hp['m_Y'])
-        # Validate the net after each epoch
-        if n and (not n % epoch_size):
-            epoch_result = collections.OrderedDict()
-            epoch_result['iteration'] = n
-            # Store current SGD cost
-            epoch_result['train_cost'] = train_cost
-            # Also compute validate cost (more stable)
-            epoch_result['validate_cost'] = cost(X_validate, X_validate_n, Y_validate, Y_validate_n, hp['alpha_XY'],
-                                                 hp['m_XY'], hp['alpha_X'], hp['m_X'], hp['alpha_Y'], hp['m_Y'])
-            
-            # Get accuracy and diagnostic figures for both train and validation sets
-            for name, X_output, Y_output in [('train', X_train_output.eval(), Y_train_output.eval()),
-                                             ('validate', X_validate_output.eval(), Y_validate_output.eval())]:
-                N = X_output.shape[1]
-                # Compute and display metrics on the resulting hashes
-                correct, in_class_mean, in_class_std = hashing_utils.statistics(X_output > 0, Y_output > 0)
-                collisions, out_of_class_mean, out_of_class_std = hashing_utils.statistics(X_output[:, np.random.permutation(N)] > 0,
-                                                                                           Y_output > 0)
-                epoch_result[name + '_accuracy'] = correct/float(N)
-                epoch_result[name + '_in_class_distance_mean'] = in_class_mean
-                epoch_result[name + '_in_class_distance_std'] = in_class_std
-                epoch_result[name + '_collisions'] = collisions/float(N)
-                epoch_result[name + '_out_of_class_distance_mean'] = out_of_class_mean
-                epoch_result[name + '_out_of_class_distance_std'] = out_of_class_std
-                epoch_result[name + '_hash_entropy_X'] = hashing_utils.hash_entropy(X_output > 0)
-                epoch_result[name + '_hash_entropy_Y'] = hashing_utils.hash_entropy(Y_output > 0)
-            if epoch_result['validate_cost'] < current_validate_cost:
-                if epoch_result['validate_cost'] < improvement_threshold*current_validate_cost:
-                    patience *= patience_increase
-                    print " ... increasing patience to {} because {} < {}*{}".format(patience,
-                                                                                     epoch_result['validate_cost'],
-                                                                                     improvement_threshold,
-                                                                                     current_validate_cost)
-                current_validate_cost = epoch_result['validate_cost']
-            # Only compute MRR on validate
-            mrr_pessimist, mrr_optimist = hashing_utils.mean_reciprocal_rank(X_output[:, mrr_samples] > 0,
-                                                                             Y_output > 0,
-                                                                             mrr_samples)
-            epoch_result['validate_mrr_pessimist'] = mrr_pessimist
-            epoch_result['validate_mrr_optimist'] = mrr_optimist
-
-            epoch_results.append(epoch_result)
-            print '    patience : {}'.format(patience)
-            print '    current_validation_cost : {}'.format(current_validate_cost)
-            for k, v in epoch_result.items():
-                print '    {} : {}'.format(k, round(v, 3))
-            print
-        if n > patience:
-            break
     with open(os.path.join(trial_directory, 'epochs.pkl'), 'wb') as f:
+        pickle.dump(epoch_results, f)
+    with open(os.path.join(trial_directory, 'parameters.pkl'), 'wb') as f:
         pickle.dump(epoch_results, f)
 
