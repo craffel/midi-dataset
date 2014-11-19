4111f9588a5d96a68efe72752fdfd088c1bc7371
Colin Raffel <craffel@gmail.com>
2014-11-18 23:50:35 -0500

    Switching to using echonest for artist

--- query_freebase.py
+++ query_freebase.py
@@ -1,92 +1,112 @@
 import json
 import urllib
 import unicodedata
-API_KEY = 'AIzaSyAMo_6FhlxCgdTXq5lo9_hm4zLCXtEETOc'
-FREEBASE_URL = 'https://www.googleapis.com/freebase/v1/search'
+import pyen
+import collections
+FREEBASE_KEY = open('.freebase_key').read()
+ECHONEST_KEY = open('.echonest_key').read()
+FREEBASE_URL = 'https://www.googleapis.com/freebase/v1/search?'
 
 
-def normalize(artists, titles):
+def clean(string):
     '''
-    Normalize an artist/title pair using freebase
+    Removes non-ascii characters from a string in a semi-smart way
 
     :parameters:
-        - artist : str or list of str
+        - string : str or unicode
+            String to clean
+
+    :returns:
+        - clean_string : str
+            ASCII string
+    '''
+    if type(string) == str:
+        string = unicode(string, 'utf-8', 'ignore')
+    return unicodedata.normalize('NFKD', string).encode('ascii', 'ignore')
+
+
+def echonest_normalize_artist(artists):
+    '''
+    Normalize artist names using echonest
+
+    :parameters:
+        - artists : str or list of str
             Query artist name or list of potential artist names
-        - title : str or list of str
+        - titles : str or list of str
+            Query title or list of potential song titles
+
+    :returns:
+        - artists : list of str
+            Unique list of matching artists
+    '''
+    if type(artists) == str or type(artists) == unicode:
+        artists = [artists]
+
+    matched_artists = []
+
+    en = pyen.Pyen(api_key=ECHONEST_KEY)
+
+    for query_artist in artists:
+        response = en.get('artist/search',
+                          name=clean(query_artist),
+                          results=5,
+                          fuzzy_match='true')
+        if len(response['artists']) > 0:
+            for matched_artist in response['artists']:
+                matched_artists.append(matched_artist['name'])
+    if len(matched_artists) == 0:
+        return None
+    matched_artists = list(collections.OrderedDict.fromkeys(matched_artists))
+    return matched_artists
+
+
+def freebase_normalize_title(artists, titles):
+    '''
+    Normalize a song title using freebase
+
+    :parameters:
+        - artists : str or list of str
+            Query artist name or list of potential artist names
+        - titles : str or list of str
             Query title or list of potential song titles
 
     :returns:
         - artist : str or NoneType
-            Freebase's purported artist name or None if no match
+            Freebase's chosen title out of the supplied list or None if no match
         - title: str or NoneType
             Freebase's purported title or None if no match
     '''
-    def clean(string):
-        ''' Removes non-ascii characters from a string '''
-        if type(string) == str:
-            string = unicode(string, 'utf-8', 'ignore')
-        return unicodedata.normalize('NFKD', string).encode('ascii', 'ignore')
-
-    def artist_match(artist, old_correction=None):
-        ''' Match an artist using freebase '''
-        filter_str = '(all name:"{}" type:/music/artist)'
-        params = {'query': clean(artist),
-                  'filter': filter_str.format(clean(artist)),
-                  'limit': 1,
-                  'key': API_KEY,
-                  'spell': 'always'}
-        url = FREEBASE_URL + '?' + urllib.urlencode(params)
-        response = json.loads(urllib.urlopen(url).read())
-        if 'correction' in response:
-            correction = response['correction'][0]
-            if correction == old_correction:
-                return correction
-            else:
-                return artist_match(response['correction'][0], correction)
-        if len(response['result']) > 0:
-            return response['result'][0]['name']
-        else:
-            return None
-
     def title_match(artist, title, old_correction=None):
         ''' Match a song title with some artist using freebase '''
         filter_str = '(all type:/music/recording /music/recording/artist:"{}")'
         params = {'query': clean(title),
-                  'filter': filter_str.format(clean(artist)),
+                  'filter': filter_str.format(clean(artist).replace('"', '')),
                   'limit': 1,
-                  'key': API_KEY,
+                  'key': FREEBASE_KEY,
                   'spell': 'always'}
-        url = FREEBASE_URL + '?' + urllib.urlencode(params)
+        url = FREEBASE_URL + urllib.urlencode(params)
         response = json.loads(urllib.urlopen(url).read())
+        if len(response['result']) > 0:
+            return response['result'][0]['name']
         if 'correction' in response:
             correction = response['correction'][0]
             if correction == old_correction:
-                return correction
+                return None
             else:
                 return title_match(artist,
                                    response['correction'][0],
                                    correction)
-        if len(response['result']) > 0:
-            return response['result'][0]['name']
-        else:
-            return None
+        return None
 
     if type(artists) == str or type(artists) == unicode:
         artists = [artists]
 
-    for query_artist in artists:
-        artist = artist_match(query_artist)
-        if artist is not None:
-            break
-    if artist is None:
-        return None, None
-
     if type(titles) == str or type(artists) == unicode:
         titles = [titles]
 
-    for query_title in titles:
-        title = title_match(artist, query_title)
-        if title is not None:
-            break
-
-    return artist, title
+    for query_artist in artists:
+        for query_title in titles:
+            title = title_match(query_artist, query_title)
+            if title is not None:
+                return query_artist, title
+    return None, None
