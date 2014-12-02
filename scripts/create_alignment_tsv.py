a6c4d2e71b2096575abdd737a3f5c8b934ef595d
Colin Raffel <craffel@gmail.com>
2014-12-02 12:07:02 -0500

    Not saving the sim mtx so we have to reconstruct it

--- scripts/create_alignment_tsv.py
+++ scripts/create_alignment_tsv.py
@@ -10,6 +10,8 @@ import alignment_analysis
 import os
 import json
 import numpy as np
+import scipy.spatial
+import joblib
 
 BASE_DATA_PATH = '../data/'
 ALIGNMENT_FOLDER = 'clean_midi_aligned'
@@ -38,7 +40,11 @@ def process_one_file(diagnostics_file):
     midi_entry = midi_entry[0]
     midi_name = '{} - {}'.format(midi_entry['artist'], midi_entry['title'])
     diagnostics = np.load(diagnostics_file)
-    scores = alignment_analysis.get_scores(diagnostics['similarity_matrix'],
+    midi_gram = np.load(str(diagnostics['midi_features_filename']))['gram']
+    audio_gram = np.load(str(diagnostics['audio_features_filename']))['gram']
+    similarity_matrix = scipy.spatial.distance.cdist(midi_gram.T, audio_gram.T,
+                                                     metric='cosine')
+    scores = alignment_analysis.get_scores(similarity_matrix,
                                            diagnostics['p'], diagnostics['q'],
                                            float(diagnostics['score']))
     row = [diagnostics_file, dataset, dataset_id, midi_md5, audio_name,
@@ -47,6 +53,10 @@ def process_one_file(diagnostics_file):
 
 npz_path = os.path.join(BASE_DATA_PATH, ALIGNMENT_FOLDER, 'npz', '*.npz')
 tsv_file = os.path.join(BASE_DATA_PATH, ALIGNMENT_FOLDER, 'results.tsv')
+
+results = joblib.Parallel(n_jobs=10)(joblib.delayed(process_one_file)(filename)
+                                     for filename in glob.glob(npz_path))
+
 with open(tsv_file, 'wb') as f:
-    for diagnostics_file in glob.glob(npz_path):
-        f.write('\t'.join(process_one_file(diagnostics_file)) + '\n')
+    for result in results:
+        f.write('\t'.join(result) + '\n')
