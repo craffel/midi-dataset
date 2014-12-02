41e435e6ada80bba5ee8df322c36afba6e69f7c3
Colin Raffel <craffel@gmail.com>
2014-12-02 12:07:20 -0500

    Code for creating a uspop2002 file list with echonest liveness

---
+++ scripts/get_live_uspop2002.py
@@ -0,0 +1,39 @@
+import pyen
+import sys
+sys.path.append('../')
+import os
+import time
+
+ECHONEST_KEY = open('../.echonest_key').read()
+BASE_DATA_PATH = '../data/'
+
+en = pyen.Pyen(ECHONEST_KEY)
+
+
+def wait_for_analysis(id):
+    while True:
+        response = en.get('track/profile', id=id, bucket=['audio_summary'])
+        if response['track']['status'] != 'pending':
+            break
+        time.sleep(1)
+
+    return response['track']['audio_summary']
+
+uspop_tracks = []
+with open('../file_lists/uspop2002.txt') as f:
+    for line in f:
+        uspop_tracks.append(line.rstrip().split('\t'))
+
+with open('../file_lists/uspop2002_liveness.txt', 'wb') as tsv_file:
+    for track in uspop_tracks:
+        filename = os.path.join(BASE_DATA_PATH, 'uspop2002', 'mp3', track[-1])
+        with open(filename, 'rb') as f:
+            result = en.post('track/upload', track=f, filetype='mp3')
+        try:
+            track_id = result['track']['id']
+            result = wait_for_analysis(track_id)
+            line = track + [str(result['liveness'])]
+            print line
+            tsv_file.write('\t'.join(line) + '\n')
+        except Exception as e:
+            print '  ## Bad result: {}\n  {}'.format(filename, e.message)
