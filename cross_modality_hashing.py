6e966582ae8f77590da8a6d4afde83db63b4f91c
Colin Raffel <craffel@gmail.com>
2014-12-23 00:21:54 -0500

    Removing params

--- cross_modality_hashing.py
+++ cross_modality_hashing.py
@@ -22,9 +22,9 @@ import collections
 
 def train_cross_modality_hasher(X_train, Y_train, X_validate, Y_validate,
                                 hidden_layer_sizes_X, hidden_layer_sizes_Y,
-                                alpha_XY, m_XY, alpha_X, m_X, alpha_Y, m_Y,
-                                n_bits, dropout=False, learning_rate=1e-5,
-                                momentum=.9, batch_size=100, epoch_size=1000,
+                                alpha_XY, m_XY, n_bits, dropout=False,
+                                learning_rate=1e-5, momentum=.9,
+                                batch_size=100, epoch_size=1000,
                                 initial_patience=10000,
                                 improvement_threshold=0.995,
                                 patience_increase=1.1, max_iter=200000,
@@ -52,14 +52,6 @@ def train_cross_modality_hasher(X_train, Y_train, X_validate, Y_validate,
             Scaling parameter for cross-modality negative example cost
         - m_XY : int
             Cross-modality negative example threshold
-        - alpha_X : float
-            Scaling parameter for X-modality negative example cost
-        - m_X : int
-            Y-modality negative example threshold
-        - alpha_Y : float
-            Scaling parameter for Y-modality negative example cost
-        - m_Y : int
-            Y-modality negative example threshold
         - n_bits : int
             Number of bits in the output representation
         - dropout : bool
