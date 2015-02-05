b38828c59eea9fc7d1f2dae5261f42e876078ff5
Colin Raffel <craffel@gmail.com>
2015-02-04 21:14:25 -0500

    Parallelizing

--- scripts/hash_msd.py
+++ scripts/hash_msd.py
@@ -13,6 +13,7 @@ import os
 import numpy as np
 import theano
 import theano.tensor as T
+import joblib
 
 BASE_DATA_PATH = '../data'
 output_path = os.path.join(BASE_DATA_PATH, 'msd', 'pkl')
@@ -29,34 +30,56 @@ hash = theano.function([hasher_input],
                        hasher_layers[-1].get_output(hasher_input,
                                                     deterministic=True))
 
-h5_glob = os.path.join(BASE_DATA_PATH, 'msd', 'h5', '*', '*', '*.h5')
-for h5_file in glob.glob(h5_glob):
-    with hdf5_getters.open_h5_file_read(h5_file) as h5:
-        # Load in beat times from MSD
-        beats = hdf5_getters.get_beats_start(h5)
-        artist = str(hdf5_getters.get_artist_name(h5))
-        title = str(hdf5_getters.get_title(h5))
-        duration = hdf5_getters.get_duration(h5)
-        # Some files have no EN analysis
-        if beats.size == 0:
-            continue
-        # and beat-synchronous feature matrices
-        chroma = beat_aligned_feats.get_btchromas(h5)
-        timbre = beat_aligned_feats.get_bttimbre(h5)
-        loudness = beat_aligned_feats.get_btloudnessmax(h5)
-    msd_features = np.vstack([chroma, timbre, loudness])
-    msd_features = msd_features.T
-    msd_features = hashing_utils.shingle(msd_features, 4)
-    mean, std = hashing_utils.standardize(msd_features)
-    msd_features = (msd_features - mean)/std
-    if np.isnan(msd_features).any():
-        continue
-    hashed_features = hash(msd_features.astype(theano.config.floatX))
-    hashes = hash_match.vectors_to_ints(hashed_features > 0).astype('uint16')
-    h5_dict = {'artist': artist, 'title': title,
-               'hash_list': hashes, 'duration': duration}
-    output_filename = h5_file.replace('h5', 'pkl')
-    if not os.path.exists(os.path.split(output_filename)[0]):
-        os.makedirs(os.path.split(output_filename)[0])
-    with open(output_filename, 'wb') as f:
-        pickle.dump(h5_dict, f)
+h5_glob = os.path.join(BASE_DATA_PATH, 'msd', 'h5', '*', '*', '*', '*.h5')
+
+
+def process_one_file(h5_file):
+    '''
+    Hash the features in a single h5 file.
+
+    :parameters:
+        - h5_file : str
+            Path to h5 file to process
+    '''
+    try:
+        output_filename = h5_file.replace('h5', 'pkl')
+        if os.path.exists(output_filename):
+            return
+        with hdf5_getters.open_h5_file_read(h5_file) as h5:
+            # Load in beat times from MSD
+            beats = hdf5_getters.get_beats_start(h5)
+            artist = str(hdf5_getters.get_artist_name(h5))
+            title = str(hdf5_getters.get_title(h5))
+            duration = hdf5_getters.get_duration(h5)
+            # Some files have no EN analysis
+            if beats.size == 0:
+                return
+            # and beat-synchronous feature matrices
+            chroma = beat_aligned_feats.get_btchromas(h5)
+            timbre = beat_aligned_feats.get_bttimbre(h5)
+            loudness = beat_aligned_feats.get_btloudnessmax(h5)
+        msd_features = np.vstack([chroma, timbre, loudness])
+        msd_features = msd_features.T
+        if msd_features.shape[0] < 5:
+            return
+        msd_features = hashing_utils.shingle(msd_features, 4)
+        mean, std = hashing_utils.standardize(msd_features)
+        msd_features = (msd_features - mean)/std
+        if np.isnan(msd_features).any():
+            return
+        hashed_features = hash(msd_features.astype(theano.config.floatX))
+        hashes = hash_match.vectors_to_ints(hashed_features > 0)
+        hashes = hashes.astype('uint16')
+        h5_dict = {'artist': artist, 'title': title,
+                'hash_list': hashes, 'duration': duration}
+        output_filename = h5_file.replace('h5', 'pkl')
+        if not os.path.exists(os.path.split(output_filename)[0]):
+            os.makedirs(os.path.split(output_filename)[0])
+        with open(output_filename, 'wb') as f:
+            pickle.dump(h5_dict, f)
+    except:
+        print "Error creating {}".format(h5_file)
+        return
+
+joblib.Parallel(n_jobs=10, verbose=1)(joblib.delayed(process_one_file)(h5_file)
+                                      for h5_file in glob.glob(h5_glob))
