184716dd84a0c648746c2c88d1b074e4a01fa84b
Colin Raffel <craffel@gmail.com>
2015-03-25 20:46:44 -0400

    Adding dev set, consisting of matches in cal10k/cal500/uspop2002

--- scripts/create_test_set.py
+++ scripts/create_test_set.py
@@ -33,26 +33,38 @@ for dataset in DATASETS:
             if len(matches) > 0:
                 midis_in_datasets.append(midi_entry)
 
-# Remove these from the MIDI list
-for entry in midis_in_datasets:
-    try:
-        del midi_list[midi_list.index(entry)]
-    except ValueError:
-        continue
-
-# List of MIDI-MSD matches
-midi_msd = []
 # Load the whoosh index for the MSD
 index_path = os.path.join(BASE_DATA_PATH, 'msd', 'index')
 index = whoosh_search.get_whoosh_index(index_path)
+# List of MIDI-MSD matches for the development set
+dev_set = []
+with index.searcher() as searcher:
+    for entry in midis_in_datasets:
+        # Match each MIDI file entry against the MSD
+        matches = whoosh_search.search(searcher, index.schema,
+                                       entry['artist'],
+                                       entry['title'])
+        for match in matches:
+            dev_set.append([entry['md5'], match[0]])
+        # Remove these from the MIDI list for test set
+        try:
+            del midi_list[midi_list.index(entry)]
+        except ValueError:
+            continue
+
+with open(os.path.join(FILE_LIST_PATH, 'dev_pairs.csv'), 'wb') as f:
+    f.write('\n'.join(['{},{}'.format(*match) for match in dev_set]))
+
+# List of MIDI-MSD matches
+test_set = []
 with index.searcher() as searcher:
     for midi_entry in midi_list:
-        # Match each MIDI file entry against this dataset
+        # Match each MIDI file entry against the MSD
         matches = whoosh_search.search(searcher, index.schema,
                                        midi_entry['artist'],
                                        midi_entry['title'])
         for match in matches:
-            midi_msd.append([midi_entry['md5'], match[0]])
+            test_set.append([midi_entry['md5'], match[0]])
 
 with open(os.path.join(FILE_LIST_PATH, 'test_pairs.csv'), 'wb') as f:
-    f.write('\n'.join(['{},{}'.format(*match) for match in midi_msd]))
+    f.write('\n'.join(['{},{}'.format(*match) for match in test_set]))
