2b8bbab6fd6d4b6f9282783295ba5c36c4f5777a
Colin Raffel <craffel@gmail.com>
2015-12-03 22:26:50 -0500

    Update text matching to use whoosh only

--- scripts/text_match_datasets.py
+++ scripts/text_match_datasets.py
@@ -11,10 +11,13 @@ import os
 # The datasets to match MIDIs against
 DATASETS = ['cal10k', 'cal500', 'uspop2002', 'msd']
 BASE_DATA_PATH = '../data'
+RESULTS_PATH = '../results'
 
 # Load in list of MIDI files
-with open(os.path.join(BASE_DATA_PATH, 'clean_midi', 'index.js')) as f:
-    midi_list = json.load(f)
+midi_index = whoosh_search.get_whoosh_index(
+    os.path.join(BASE_DATA_PATH, 'clean_midi', 'index'))
+with midi_index.searcher() as searcher:
+    midi_list = list(searcher.documents())
 
 # Create dict of whoosh indices
 indices = {}
@@ -49,6 +52,9 @@ def merge_entries(indices):
     for index in indices[:-1]:
         del pairs[index]
 
+# Pre-construct all searchers (for speed)
+searchers = {dataset: indices[dataset].searcher() for dataset in DATASETS}
+
 # We will remove used entries from the MIDI list as we go until there are none
 while len(midi_list) > 0:
     # Pop the last entry off the list
@@ -66,17 +72,16 @@ while len(midi_list) > 0:
     # Match each of these MIDIs against each dataset
     dataset_matches = []
     for dataset in DATASETS:
-        with indices[dataset].searcher() as searcher:
-            matches = whoosh_search.search(
-                searcher, indices[dataset].schema, midi_entry['artist'],
-                midi_entry['title'])
-            # Add the each matched dataset entry in if we haven't already
-            for match in matches:
-                if [dataset, match[0]] not in dataset_matches:
-                    dataset_matches.append([dataset, match[0]])
+        matches = whoosh_search.search(
+            searchers[dataset], indices[dataset].schema, midi_entry['artist'],
+            midi_entry['title'])
+        # Add the each matched dataset entry in if we haven't already
+        for match in matches:
+            if [dataset, match[0]] not in dataset_matches:
+                dataset_matches.append([dataset, match[0]])
     # If there are any matches, add them to pairs
     if len(dataset_matches) > 0:
-        pairs.append([[m['md5'] for m in midi_matches], dataset_matches])
+        pairs.append([[m['id'] for m in midi_matches], dataset_matches])
         # Find other pairs which have include one of these dataset entries
         merge_indices = []
         for n, pair in enumerate(pairs):
@@ -90,5 +95,9 @@ while len(midi_list) > 0:
         if len(merge_indices) > 1:
             merge_entries(merge_indices)
 
-with open('../file_lists/text_matches.js', 'wb') as f:
+# Close all searchers
+for dataset, searcher in searchers.items():
+    searcher.close()
+
+with open(os.path.join(RESULTS_PATH, 'text_matches.js'), 'wb') as f:
     json.dump(pairs, f)
