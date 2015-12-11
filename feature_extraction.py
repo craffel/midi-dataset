eebe1ef1eb27626481475a3f9495462d8f6f11b5
Colin Raffel <craffel@gmail.com>
2015-12-11 11:00:16 -0500

    Update alignment and feature extraction
    * No longer beat-synchronizing
    * Using h5/hickle for caching features
    * Updating alignment parameters to match alignment-search

---
+++ feature_extraction.py
@@ -0,0 +1,151 @@
+'''
+Utilities for feature extraction.
+'''
+
+import numpy as np
+import tempfile
+import subprocess
+import os
+import pretty_midi
+import librosa
+
+AUDIO_FS = 22050
+AUDIO_HOP = 1024
+MIDI_FS = 11025
+MIDI_HOP = 512
+NOTE_START = 36
+N_NOTES = 48
+
+
+def fast_fluidsynth(m, fs):
+    '''
+    Faster fluidsynth synthesis using the command-line program
+    instead of pyfluidsynth.
+
+    Parameters
+    ----------
+    - m : pretty_midi.PrettyMIDI
+        Pretty MIDI object
+    - fs : int
+        Sampling rate
+
+    Returns
+    -------
+    - midi_audio : np.ndarray
+        Synthesized audio, sampled at fs
+    '''
+    # Write out temp mid file
+    temp_mid = tempfile.NamedTemporaryFile()
+    m.write(temp_mid.name)
+    # Get path to temporary .wav file
+    temp_wav = tempfile.NamedTemporaryFile()
+    # Get path to default pretty_midi SF2
+    sf2_path = os.path.join(os.path.dirname(pretty_midi.__file__),
+                            pretty_midi.DEFAULT_SF2)
+    # Make system call to fluidsynth
+    with open(os.devnull, 'w') as devnull:
+        subprocess.check_output(
+            ['fluidsynth', '--fast-render={}'.format(temp_wav.name),
+             '-r', str(fs), sf2_path, temp_mid.name], stderr=devnull)
+    # Load from temp wav file
+    audio, _ = librosa.load(temp_wav.name, sr=fs)
+    # Close/delete temp files
+    temp_mid.close()
+    temp_wav.close()
+    return audio
+
+
+def midi_cqt(midi_object):
+    '''
+    Synthesize MIDI data, compute its constant-Q spectrogram, normalize, and
+    log-scale it
+
+    Parameters
+    ----------
+    midi_object : pretty_midi.PrettyMIDI
+        MIDI data to create constant-Q spectrogram of.
+
+    Returns
+    -------
+    midi_gram : np.ndarray
+        Log-magnitude, L2-normalized constant-Q spectrugram of synthesized MIDI
+        data.
+    '''
+    # Synthesize MIDI object as audio data
+    midi_audio = fast_fluidsynth(midi_object, MIDI_FS)
+    # Compute CQT of the synthesized audio data
+    midi_gram = librosa.cqt(
+        midi_audio, sr=MIDI_FS, hop_length=MIDI_HOP,
+        fmin=librosa.midi_to_hz(NOTE_START), n_bins=N_NOTES)
+    # L2-normalize and log-magnitute it
+    return post_process_cqt(midi_gram)
+
+
+def audio_cqt(audio_data, fs=AUDIO_FS):
+    '''
+    Compute some audio data's constant-Q spectrogram, normalize, and log-scale
+    it
+
+    Parameters
+    ----------
+    audio_data : np.ndarray
+        Some audio signal.
+    fs : int
+        Sampling rate the audio data is sampled at, should be ``AUDIO_FS``.
+
+    Returns
+    -------
+    midi_gram : np.ndarray
+        Log-magnitude, L2-normalized constant-Q spectrugram of synthesized MIDI
+        data.
+    '''
+    # Compute CQT of the synthesized audio data
+    audio_gram = librosa.cqt(
+        audio_data, sr=fs, hop_length=AUDIO_HOP,
+        fmin=librosa.midi_to_hz(NOTE_START), n_bins=N_NOTES)
+    # L2-normalize and log-magnitute it
+    return post_process_cqt(audio_gram)
+
+
+def post_process_cqt(gram):
+    '''
+    Normalize and log-scale a Constant-Q spectrogram
+
+    Parameters
+    ----------
+    gram : np.ndarray
+        Constant-Q spectrogram, constructed from ``librosa.cqt``.
+
+    Returns
+    -------
+    log_normalized_gram : np.ndarray
+        Log-magnitude, L2-normalized constant-Q spectrogram.
+    '''
+    # Compute log amplitude
+    gram = librosa.logamplitude(gram, ref_power=gram.max())
+    # Transpose so that rows are samples
+    gram = gram.T
+    # and L2 normalize
+    gram = librosa.util.normalize(gram, norm=2., axis=1)
+    return gram
+
+
+def frame_times(gram):
+    '''
+    Get the times corresponding to the frames in a spectrogram, which was
+    created with one of the functions here.
+
+    Parameters
+    ----------
+    gram : np.ndarray
+        Spectrogram matrix.
+
+    Returns
+    -------
+    times : np.ndarray
+        Time, in seconds, of each frame in gram.
+    '''
+    # Note that because MIDI_FS = AUDIO_FS/2 and MIDI_HOP = AUDIO_HOP/2, using
+    # AUDIO_FS and AUDIO_HOP here works whether it's a "MIDI" or "audio" cqt.
+    return librosa.frames_to_time(
+        np.arange(gram.shape[0]), AUDIO_FS, AUDIO_HOP)
---
+++ scripts/align_text_matches.py
@@ -0,0 +1,304 @@
+import numpy as np
+import librosa
+import pretty_midi
+import joblib
+import os
+import sys
+sys.path.append('..')
+import itertools
+import json
+import feature_extraction
+import djitw
+import hickle
+import whoosh_search
+import traceback
+
+BASE_DATA_PATH = '../data/'
+MIDI_FOLDER = 'clean_midi'
+DATASETS = ['cal10k', 'cal500', 'uspop2002', 'msd']
+RESULTS_PATH = '../results/'
+OUTPUT_FOLDER = 'clean_midi_aligned'
+
+
+def path_to_file(base_path, basename, extension):
+    '''
+    Returns the path to an actual file given the dataset, base file name,
+    and file extension.  Assumes the dataset format of
+    BASE_DATA_PATH/dataset/extension/basename.extension
+
+    Parameters
+    ----------
+    base_path : str
+        Base path, should be e.g. os.path.join(BASE_DATA_PATH, 'uspop2002')
+    basename : str
+        Base name of the file
+    extension : str
+        Extension of the file, e.g. mp3, h5, mid
+
+    Returns
+    -------
+    full_file_path : str
+        Full path to the file in question.
+    '''
+    return os.path.join(base_path, extension,
+                        '{}.{}'.format(basename, extension))
+
+
+def check_subdirectories(filename):
+    '''
+    Checks that the subdirectories up to filename exist; if they don't, create
+    them.
+
+    Parameters
+    ----------
+    filename : str
+        Full path to file
+    '''
+    if not os.path.exists(os.path.split(filename)[0]):
+        os.makedirs(os.path.split(filename)[0])
+
+
+def align_one_file(audio_filename, midi_filename, audio_features_filename=None,
+                   midi_features_filename=None, output_midi_filename=None,
+                   output_diagnostics_filename=None):
+    '''
+    Helper function for aligning a MIDI file to an audio file.
+
+    Parameters
+    ----------
+    audio_filename : str
+        Full path to an audio file.
+    midi_filename : str
+        Full path to a midi file.
+    audio_features_filename : str or None
+        Full path to pre-computed features for the audio file.
+        If the file doesn't exist, features will be computed and saved.
+        If None, force re-computation of the features and don't save.
+    midi_features_filename : str or None
+        Full path to pre-computed features for the midi file.
+        If the file doesn't exist, features will be computed and saved.
+        If None, force re-computation of the features and don't save.
+    output_midi_filename : str or None
+        Full path to where the aligned .mid file should be written.
+        If None, don't output.
+    output_diagnostics_filename : str or None
+        Full path to a file to write out diagnostic information (similarity
+        matrix, best path, etc) in a .h5 file.  If None, don't output.
+
+    Returns
+    -------
+    p, q : np.ndarray
+        Indices of the lowest-cost alignment between the audio and MIDI
+    score : float
+        Normalized DTW path distance
+    '''
+    # Skip when already processed
+    if (output_diagnostics_filename is not None
+            and os.path.exists(output_diagnostics_filename)):
+        return
+
+    try:
+        m = pretty_midi.PrettyMIDI(midi_filename)
+    except Exception as e:
+        print 'Could not parse {}: {}'.format(
+            os.path.split(midi_filename)[1], traceback.format_exc(e))
+        return
+
+    midi_features = {}
+
+    # If a feature file was provided and the file exists, try to read it in
+    if (midi_features_filename is not None and
+            os.path.exists(midi_features_filename)):
+        try:
+            # If a feature file was provided and exists, read it in
+            midi_features = hickle.load(midi_features_filename)
+        # If there was a problem reading, force re-cration
+        except Exception as e:
+            print "Error reading {}: {}".format(
+                midi_features_filename, traceback.format_exc(e))
+            midi_features = {}
+
+    if not midi_features:
+        # Generate synthetic MIDI CQT
+        try:
+            midi_features['gram'] = feature_extraction.midi_cqt(m)
+        except Exception as e:
+            print "Error creating CQT for {}: {}".format(
+                os.path.split(midi_filename)[1], traceback.format_exc(e))
+            return
+        if midi_features_filename is not None:
+            try:
+                # Write out
+                check_subdirectories(midi_features_filename)
+                hickle.dump(
+                    midi_features, midi_features_filename, compression='gzip')
+            except Exception as e:
+                print "Error writing {}: {}".format(
+                    os.path.split(midi_filename)[1], traceback.format_exc(e))
+                return
+
+    audio_features = {}
+
+    # If a feature file was provided and the file exists, try to read it in
+    if (audio_features_filename is not None and
+            os.path.exists(audio_features_filename)):
+        # If a feature file was provided and exists, read it in
+        try:
+            audio_features = hickle.load(audio_features_filename)
+        # If there was a problem reading, force re-cration
+        except Exception as e:
+            print "Error reading {}: {}".format(
+                audio_features_filename, traceback.format_exc(e))
+            audio_features = {}
+
+    # Cache audio CQT
+    if not audio_features:
+        try:
+            # Read in audio data
+            audio, fs = librosa.load(
+                audio_filename, sr=feature_extraction.AUDIO_FS)
+            # Compute audio cqt
+            audio_features['gram'] = feature_extraction.audio_cqt(audio)
+        except Exception as e:
+            print "Error creating CQT for {}: {}".format(
+                os.path.split(audio_filename)[1], traceback.format_exc(e))
+            return
+        if audio_features_filename is not None:
+            try:
+                # Write out
+                check_subdirectories(audio_features_filename)
+                hickle.dump(audio_features, audio_features_filename,
+                            compression='gzip')
+            except Exception as e:
+                print "Error writing {}: {}".format(
+                    os.path.split(audio_filename)[1], traceback.format_exc(e))
+                return
+
+    try:
+        # Check that the similarity matrix will not be too big before computing
+        size = midi_features['gram'].shape[0]*audio_features['gram'].shape[0]
+        # If > 1 GB, skip
+        if (size*64/8e9 > 1):
+            print (
+                "Similarity matrix for {} and {} would be {} GB because the "
+                "CQTs have shape {} and {}".format(
+                    os.path.split(audio_filename)[1],
+                    os.path.split(midi_filename)[1],
+                    size*64/8e9, audio_features['gram'].shape[0],
+                    midi_features['gram'].shape[0]))
+            return
+
+        # Get similarity matrix
+        similarity_matrix = 1 - np.dot(
+            midi_features['gram'], audio_features['gram'].T)
+        # Non-diagonal additive path penalty is the median of the sim mtx
+        add_pen = np.median(similarity_matrix)
+        # Get best path through matrix
+        p, q, score = djitw.dtw(similarity_matrix, gully=.96,
+                                additive_penalty=add_pen, inplace=False)
+        # Normalize score by path length
+        score /= float(len(p))
+        # Normalize score by score by mean sim matrix value within path chunk
+        score /= similarity_matrix[p.min():p.max(), q.min():q.max()].mean()
+    except Exception as e:
+        print "Error performing DTW for {} and {}: {}".format(
+            os.path.split(audio_filename)[1],
+            os.path.split(midi_filename)[1],
+            traceback.format_exc(e))
+        return
+
+    # Write out the aligned file
+    if output_midi_filename is not None:
+        try:
+            # Adjust MIDI timing
+            m.adjust_times(
+                feature_extraction.frame_times(midi_features['gram'])[p],
+                feature_extraction.frame_times(audio_features['gram'])[q])
+            check_subdirectories(output_midi_filename)
+            m.write(output_midi_filename)
+        except Exception as e:
+            print "Error writing aligned .mid for {}: {}".format(
+                os.path.split(midi_filename)[1], traceback.format_exc(e))
+            return
+
+    if output_diagnostics_filename is not None:
+        try:
+            check_subdirectories(output_diagnostics_filename)
+            diagnostics = dict(
+                p=p, q=q, score=score,
+                audio_filename=os.path.abspath(audio_filename),
+                midi_filename=os.path.abspath(midi_filename),
+                audio_features_filename=os.path.abspath(
+                    audio_features_filename),
+                midi_features_filename=os.path.abspath(midi_features_filename),
+                output_midi_filename=os.path.abspath(output_midi_filename),
+                output_diagnostics_filename=os.path.abspath(
+                    output_diagnostics_filename))
+            hickle.dump(
+                diagnostics, output_diagnostics_filename, compression='gzip')
+        except Exception as e:
+            print "Error writing diagnostics for {} and {}: {}".format(
+                os.path.split(audio_filename)[1],
+                os.path.split(midi_filename)[1], traceback.format_exc(e))
+            return
+    return p, q, score
+
+
+if __name__ == '__main__':
+    # Create the output dir if it doesn't exist
+    output_path = os.path.join(RESULTS_PATH, OUTPUT_FOLDER)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    if not os.path.exists(os.path.join(output_path, 'mid')):
+        os.makedirs(os.path.join(output_path, 'mid'))
+    if not os.path.exists(os.path.join(output_path, 'h5')):
+        os.makedirs(os.path.join(output_path, 'h5'))
+
+    # Create feature file subdirectories if they don't exist
+    for dataset in DATASETS + [MIDI_FOLDER]:
+        if not os.path.exists(os.path.join(BASE_DATA_PATH, dataset, 'h5')):
+            os.makedirs(os.path.join(BASE_DATA_PATH, dataset, 'h5'))
+
+    file_lists = {}
+    for dataset in DATASETS + [MIDI_FOLDER]:
+        # Load in whoosh index
+        index = whoosh_search.get_whoosh_index(
+            os.path.join(BASE_DATA_PATH, dataset, 'index'))
+        # Create all documents
+        with index.searcher() as searcher:
+            file_lists[dataset] = list(searcher.documents())
+        # Allow lookup by ID
+        file_lists[dataset] = dict((e['id'], e) for e in file_lists[dataset])
+
+    # Load in pairs file
+    with open(os.path.join(RESULTS_PATH, 'text_matches.js')) as f:
+        text_matches = json.load(f)
+    flattened_matches = sum([list(itertools.product(*match))
+                            for match in text_matches], [])
+
+    # Construct a list of MIDI-audio matches, which will be attempted
+    pairs = []
+    for midi_md5, (dataset, id) in flattened_matches:
+        # Populate arguments for each pair
+        file_basename = file_lists[dataset][id]['path']
+        midi_basename = file_lists[MIDI_FOLDER][midi_md5]['path']
+        output_basename = '{}_{}_{}'.format(dataset, id, midi_md5)
+        audio_filename = path_to_file(
+            os.path.join(BASE_DATA_PATH, dataset), file_basename, 'mp3')
+        midi_filename = path_to_file(
+            os.path.join(BASE_DATA_PATH, MIDI_FOLDER), midi_basename, 'mid')
+        audio_features_filename = path_to_file(
+            os.path.join(BASE_DATA_PATH, dataset), file_basename, 'h5')
+        midi_features_filename = path_to_file(
+            os.path.join(BASE_DATA_PATH, MIDI_FOLDER), midi_basename, 'h5')
+        output_midi_filename = path_to_file(
+            output_path, output_basename, 'mid')
+        output_diagnostics_filename = path_to_file(
+            output_path, output_basename, 'h5')
+        pairs.append((audio_filename, midi_filename, audio_features_filename,
+                      midi_features_filename, output_midi_filename,
+                      output_diagnostics_filename))
+
+    # Run alignment
+    joblib.Parallel(n_jobs=10, verbose=51)(
+        joblib.delayed(align_one_file)(*args) for args in pairs)
