c319093f9adf2b0f4ca26616c25193b567cb2f18
Colin Raffel <craffel@gmail.com>
2016-01-17 12:16:37 -0500

    Refactor to explicitly define probabilities

--- scripts/split_training_data.py
+++ scripts/split_training_data.py
@@ -10,9 +10,10 @@ import csv
 
 # Proportions of the dataset to use for each set
 # Train = 60%, validation = 5%, development = 10%, test = 25%
-TRAIN_VAL_OR_DEV_TEST = .6 + .05
-TRAIN_VAL_THRESHOLD = .6/TRAIN_VAL_OR_DEV_TEST
-DEV_TEST_THRESHOLD = .1/(1. - TRAIN_VAL_OR_DEV_TEST)
+TRAIN_PROB = .6
+VALID_PROB = .05
+DEV_PROB = .1
+TEST_PROB = .25
 
 # Directory to write out results
 RESULTS_PATH = os.path.join('..', 'results')
@@ -21,27 +22,17 @@ RESULTS_PATH = os.path.join('..', 'results')
 with open(os.path.join(RESULTS_PATH, 'text_matches.js')) as f:
     pairs = json.load(f)
 
+# List of output files to write
+output_files = [
+    'train_pairs.csv', 'valid_pairs.csv', 'dev_pairs.csv', 'test_pairs.csv']
 # Lists of training, validation, and test set files to be populated below
-train_files = []
-valid_files = []
-dev_files = []
-test_files = []
+file_sets = [[] for _ in range(len(output_files))]
 
 # Each pair corresponds to all MIDIs of a song matched to all mp3s of the song
 for pair in pairs:
-    # Draw a random number to choose train/val or dev/test
-    if np.random.rand() < TRAIN_VAL_OR_DEV_TEST:
-        # Draw a random number to decide train or val
-        if np.random.rand() < TRAIN_VAL_THRESHOLD:
-            file_set = train_files
-        else:
-            file_set = valid_files
-    else:
-        # Same for dev/test
-        if np.random.rand() < DEV_TEST_THRESHOLD:
-            file_set = dev_files
-        else:
-            file_set = test_files
+    # Choose a file set to add to according to their desired proportions
+    file_set = file_sets[np.random.choice(
+        len(file_sets), p=[TRAIN_PROB, VALID_PROB, DEV_PROB, TEST_PROB])]
     # Add in all MIDI/mp3 combinations to the chosen file list
     file_set += [[midi_md5, dataset, id]
                  for midi_md5, (dataset, id) in itertools.product(*pair)]
@@ -53,7 +44,5 @@ def write_list(filename, l):
         writer = csv.writer(f)
         writer.writerows(l)
 
-write_list(os.path.join(RESULTS_PATH, 'train_pairs.csv'), train_files)
-write_list(os.path.join(RESULTS_PATH, 'valid_pairs.csv'), valid_files)
-write_list(os.path.join(RESULTS_PATH, 'dev_pairs.csv'), dev_files)
-write_list(os.path.join(RESULTS_PATH, 'test_pairs.csv'), test_files)
+for file_set, filename in zip(file_sets, output_files):
+    write_list(os.path.join(RESULTS_PATH, filename), file_set)
