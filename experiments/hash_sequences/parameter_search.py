ff2f6845d89fb0ede6b8f374c6bd9b43f688d824
Colin Raffel <craffel@gmail.com>
2016-01-20 12:08:55 -0500

    Switch to using separate dhs library, and simple_spearmint

--- experiments/hash_sequences/parameter_search.py
+++ experiments/hash_sequences/parameter_search.py
@@ -1,62 +1,81 @@
-import sys
-sys.path.append('../')
-import cross_modality_hashing
-import hashing_utils
+'''
+Script for running a search over the best network hyperparameters.
+'''
+import dhs
 import numpy as np
 import os
-import spearmint.main
+import simple_spearmint
 import glob
 import traceback
+import hickle
+import collections
+import lasagne
+import functools
+import sys
 
-BASE_DATA_DIRECTORY = '../data'
+RESULTS_PATH = '../../results'
 
 
-def main(job_id, params):
-    # Spearmint requires (I think) all params to be passed as at least
-    # 1-dimensional arrays.  So, get the first entry to flatten.
-    for key, value in params.items():
-        params[key] = value[0]
-        if np.isscalar(params[key]):
-            params[key] = params[key].item()
+def run_trial(params):
+    # Load in data as dictionary of dictionaries
+    data = {'train': collections.defaultdict(list),
+            'valid': collections.defaultdict(list)}
+    data_directory = os.path.join(RESULTS_PATH, 'training_dataset')
+    for set in ['train', 'valid']:
+        for f in glob.glob(os.path.join(data_directory, set, 'h5', '*.h5')):
+            for k, v in hickle.load(f).items():
+                data[set][k].append(v)
 
-    hash_data_directory = os.path.join(BASE_DATA_DIRECTORY, 'hash_dataset')
-    train_list = list(glob.glob(os.path.join(
-        hash_data_directory, 'train', 'npz', '*.npz')))
-    valid_list = list(glob.glob(os.path.join(
-        hash_data_directory, 'valid', 'npz', '*.npz')))
-    # Load in the data
-    (X_train, Y_train, X_validate, Y_validate) = hashing_utils.load_data(
-        train_list, valid_list)
+    # Build networks
+    layers = {}
+    for network in ['X', 'Y']:
+        # Get # of features (last dimension) from first training sequence
+        input_shape = (None, 1, None, data['train'][network][0].shape[-1])
+        # Get training set statistics for standardization
+        input_mean = np.mean(
+            np.concatenate(data['train'][network], axis=1), axis=1)
+        input_std = np.std(
+            np.concatenate(data['train'][network], axis=1), axis=1)
+        # Use the number of convolutional layers to construct a list
+        # [16, 32 ...n_conv times]
+        num_filters = [2**(n + 4) for n in range(params['n_conv'])]
+        # First filter is 12 semitones tall, then (3 x 3) after that
+        filter_size = [(5, 12)] + [(3, 3)]*(params['n_conv'] - 1)
+        # Construct a maxpool size list
+        # [(2, 2), (2, 2), (1, 2) ...n_conv-2 times...]
+        ds = [(2, 2), (2, 2)] + [(1, 2)]*(params['n_conv'] - 2)
+        # Use the # of hidden layers and the hidden layer power to construct
+        # [2^hidden_power, 2^hidden_power, ...n_hidden times...]
+        hidden_layer_sizes = [2**11]*params['n_hidden']
+        layers[network] = dhs.build_network(
+            input_shape, input_mean, input_std, num_filters, filter_size, ds,
+            hidden_layer_sizes, params['dropout'], 16)
 
-    # Use the # of hidden layers and the hidden layer power to construct a list
-    # [2^hidden_power, 2^hidden_power, ...n_hidden times...]
-    hidden_layer_sizes = [2**11]*params['n_hidden']
-    params['hidden_layer_sizes'] = {'X': hidden_layer_sizes,
-                                    'Y': hidden_layer_sizes}
-    # Use the number of convolutional layers to construct a list
-    # [16, 32 ...n_conv times]
-    num_filters = [2**(n + 4) for n in xrange(params['n_conv'])]
-    params['num_filters'] = {'X': num_filters, 'Y': num_filters}
-    # First filter is 12 semitones tall
-    params['filter_size'] = {'X': [(5, 12)] + [(3, 3)]*(params['n_conv'] - 1),
-                            'Y': [(5, 12)] + [(3, 3)]*(params['n_conv'] - 1)}
-    # Construct a downsample list [(2, 2), (2, 2), (1, 2) ...n_conv-2 times...]
-    ds = [(2, 2), (2, 2)] + [(1, 2)]*(params['n_conv'] - 2)
-    params['ds'] = {'X': ds, 'Y': ds}
-    # Remove hidden_pow, n_hidden, and n_conv parameters
-    params = dict([(k, v) for k, v in params.items()
-                if k != 'hidden_pow' and k != 'n_hidden' and k != 'n_conv'])
-    for k, v in params.items():
-        print '\t{} : {},'.format(k, v)
-    # Train hasher
+    # Create updates-creating function
+    updates_function = functools.partial(
+        lasagne.updates.rmsprop, learning_rate=params['learning_rate'],
+        rho=params['momentum'])
+
+    print ',\n'.join(['\t{} : {}'.format(k, v) for k, v in params.items()])
+    # Create a list of epochs
     epochs = []
+    # Keep track of lowest objective found so far
+    best_objective = np.inf
     try:
-        for epoch, _, _ in cross_modality_hashing.train_cross_modality_hasher(
-                X_train, Y_train, X_validate, Y_validate, **params):
-            # Stop training of a nan training cost is encountered
+        for epoch in dhs.train(
+                data['train']['X'], data['train']['Y'], data['valid']['X'],
+                data['valid']['Y'], layers, params['alpha_XY'], params['m_XY'],
+                updates_function):
+            # Stop training if a nan training cost is encountered
             if not np.isfinite(epoch['train_cost']):
                 break
             epochs.append(epoch)
+            if epoch['validate_objective'] < best_objective:
+                best_objective = epoch['validate_objective']
+                best_epoch = epoch
+                best_model = {
+                    'X': lasagne.layers.get_all_param_values(layers['X']),
+                    'Y': lasagne.layers.get_all_param_values(layers['Y'])}
             print "{}: {}, ".format(epoch['iteration'],
                                     epoch['validate_objective']),
             sys.stdout.flush()
@@ -64,43 +83,71 @@ def main(job_id, params):
     except Exception:
         print "ERROR: "
         print traceback.format_exc()
-        return np.nan
+        return np.nan, {}, {}
     print
     # Check that all training costs were not NaN; return NaN if any were.
     success = np.all([np.isfinite(e['train_cost']) for e in epochs])
-    if len(epochs) == 0 or not success:
+    if np.isinf(best_objective) or len(epochs) == 0 or not success:
         print '    Failed to converge.'
         print
-        return np.nan
+        return np.nan, {}, {}
     else:
-        # If training was successful, find the maximum validation objective
-        best_objective = np.max([e['validate_objective'] for e in epochs])
-        best_epoch = [e for e in epochs
-                    if e['validate_objective'] == best_objective][0]
         for k, v in best_epoch.items():
             print "\t{:>35} | {}".format(k, v)
         print
-        # Negate it, as we will be minimizing the objective
-        return -best_objective
+        return best_objective, best_epoch, best_model
 
 
 if __name__ == '__main__':
     # Define hyperparameter space
     space = {
-        'n_hidden': {'type': 'INT', 'size': 1, 'min': 2, 'max': 3.},
-        'momentum': {'type': 'FLOAT', 'size': 1, 'min': 0, 'max': 1.},
-        'm_XY': {'type': 'INT', 'size': 1, 'min': 0, 'max': 8},
-        'dropout': {'type': 'INT', 'size': 1, 'min': 0, 'max': 1},
-        'learning_rate': {'type': 'FLOAT', 'size': 1,
-                          'min': .0001, 'max': .01},
-        'alpha_XY': {'type': 'FLOAT', 'size': 1, 'min': 0., 'max': 1.},
-        'n_conv': {'type': 'INT', 'size': 1, 'min': 2, 'max': 3}}
+        'n_hidden': {'type': 'int', 'min': 2, 'max': 3},
+        'momentum': {'type': 'float', 'min': 0., 'max': 1.},
+        'm_XY': {'type': 'int', 'min': 0, 'max': 8},
+        'dropout': {'type': 'int', 'min': 0, 'max': 1},
+        'learning_rate': {'type': 'float', 'min': .0001, 'max': .01},
+        'alpha_XY': {'type': 'float', 'min': 0., 'max': 1.},
+        'n_conv': {'type': 'int', 'min': 2, 'max': 3}}
 
-    # Set up spearmint options dict
-    options = {'language': 'PYTHON',
-               'main-file': os.path.basename(__file__),
-               'experiment-name': 'hashing_parameter_search',
-               'likelihood': 'GAUSSIAN',
-               'variables': space}
+    # Create parameter trials directory if it doesn't exist
+    trial_directory = os.path.join(RESULTS_PATH, 'dhs_parameter_trials')
+    if not os.path.exists(trial_directory):
+        os.makedirs(trial_directory)
+    # Same for model directory
+    model_directory = os.path.join(RESULTS_PATH, 'dhs_model')
+    if not os.path.exists(model_directory):
+        os.makedirs(model_directory)
 
-    spearmint.main.main(options, os.getcwd())
+    # Create SimpleSpearmint suggester instance
+    ss = simple_spearmint.SimpleSpearmint(space)
+
+    # Load in previous results for "warm start"
+    for trial_file in glob.glob(os.path.join(trial_directory, '*.h5')):
+        trial = hickle.load(trial_file)
+        ss.update(trial['hyperparameters'], trial['best_objective'])
+
+    # Run parameter optimization forever
+    while True:
+        # Get a new suggestion
+        suggestion = ss.suggest()
+        # Train a network with these hyperparameters
+        best_objective, best_epoch, best_model = run_trial(suggestion)
+        # Update spearmint on the result
+        ss.update(suggestion, best_objective)
+        # Write out a result file
+        trial_filename = ','.join('{}={}'.format(k, v)
+                                  for k, v in suggestion.items()) + '.h5'
+        hickle.dump(
+            {'hyperparameters': suggestion, 'best_objective': best_objective,
+             'best_epoch': best_epoch},
+            os.path.join(trial_directory, trial_filename))
+        # Also write out the entire model when the objective is the smallest
+        # We don't want to write all models; they are > 100MB each
+        if (not np.isnan(best_objective) and
+                best_objective < np.min(ss.objective_values)):
+            # Unfortunately, hickle can't handle hickling a dict of lists of
+            # np.ndarrays, so we need to write each model to different files
+            hickle.dump(best_model['X'],
+                        os.path.join(model_directory, 'best_model_X.h5'))
+            hickle.dump(best_model['Y'],
+                        os.path.join(model_directory, 'best_model_Y.h5'))
