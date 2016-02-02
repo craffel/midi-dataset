9240d33ac070cbb79b384202945fe10c3d587bc5
Colin Raffel <craffel@gmail.com>
2016-02-02 13:55:06 -0500

    Refactor matching for new conventions

--- experiments/dhs/match_msd.py
+++ experiments/dhs/match_msd.py
@@ -1,150 +1,139 @@
 '''
 Match entries in the clean MIDI subset to the Million Song Dataset
 '''
+import msgpack
+import msgpack_numpy
+msgpack_numpy.patch()
 import sys
-sys.path.append('..')
-import glob
-import cPickle as pickle
-import hash_match
+sys.path.append('../../')
+import whoosh_search
+import dhs
 import os
-import json
 import joblib
-import numpy as np
+import utils
+import deepdish
 
-# Path to the list of MIDI<->pairs file
-PAIR_FILE = '../file_lists/dev_pairs.csv'
-# Where to output the matching results
-RESULTS_FILE = '../results/dev_results_alignment_verified.js'
-# Where the aligned clean MIDI diagnostics files live
-ALIGNMENT_DIAGNOSTICS_PATH = '../data/clean_midi_aligned/npz/'
-
+RESULTS_PATH = '../../results'
+DATA_PATH = '../../data'
+# Should we use the test set or development set?
+SPLIT = 'dev'
+# DTW parameters
 GULLY = .9
 PENALTY = 4
 # Ignore all hash sequences below this length (they cause issues)
 MIN_SEQUENCE_LENGTH = 30
 # A DP score above this means the alignment is bad
-SCORE_THRESHOLD = .78
+SCORE_THRESHOLD = .5
 
 
-def path_to_id(pkl_file):
-    ''' Convert an MSD pkl file path to the MSD ID '''
-    return os.path.splitext(os.path.basename(pkl_file))[0]
-
-
-# Load in all hashed MSD sequences
-data = []
-for pkl_file in glob.glob('../data/msd/pkl/*/*/*/*.pkl'):
-    with open(pkl_file) as f:
-        try:
-            pkl = pickle.load(f)
-            if len(pkl['hash_list']) < MIN_SEQUENCE_LENGTH:
-                continue
-            pkl['id'] = path_to_id(pkl_file)
-            data.append(pkl)
-        except Exception as e:
-            print "Error loading {}: {}".format(pkl_file, e)
-
-# Create a separate list of the sequences
-sequences = [d['hash_list'] for d in data]
-# And a separate list of IDs
-msd_ids = [d['id'] for d in data]
-
-# Load in the clean MIDI index
-with open('../data/clean_midi/index.js') as f:
-    midi_index = json.load(f)
-
-
-def match_one_midi(midi_data, msd_match_indices):
+def match_one_midi(midi_data, msd_data, msd_match_indices):
     '''
     Match a MIDI sequence against the MSD and evaluate whether a good match was
     found
 
-    :parameters:
-        - midi_data : dict
-            Dict of MIDI data, including mean chroma vector, hash sequence, etc
-        - msd_match_indices : list-like of int
-            indices of entries in the MSD this MIDI should potentially match to
+    Parameters
+    ----------
+    midi_data : dict
+        Dict of MIDI data, including hash sequence
+    sequences : list of dict
+        List of MSD entries (hash sequences, metadata) to match against
+    msd_match_indices : list-like of int
+        Indices of entries in the sequences this MIDI should potentially match
 
-    :returns:
-        - results : dict
-            Dictionary with diagnostics about whether this match was successful
+    Returns
+    -------
+    results : dict
+        Dictionary with diagnostics about whether this match was successful
     '''
+    # Create a separate list of the sequences
+    msd_sequences = [d['hash_sequence'] for d in msd_data]
     # Match this MIDI sequence against MSD sequences
-    matches, scores, n_pruned_dist = hash_match.match_one_sequence(
-        midi_data['hash_list'], sequences, GULLY, PENALTY)
+    matches, scores, n_pruned_dist = dhs.match_one_sequence(
+        midi_data['hash_sequence'], msd_sequences, GULLY, PENALTY, prune=False)
     # Store results of the match
     results = {}
-    results['midi_md5'] = midi_data['md5']
-    results['msd_match_ids'] = [data[n]['id'] for n in msd_match_indices]
+    results['midi_md5'] = midi_data['id']
+    results['msd_match_ids'] = [msd_data[n]['id'] for n in msd_match_indices]
     # Compile the rank and score for each MSD entry which should match the MIDI
-    matched_ranks = []
-    matched_scores = []
-    for msd_index in msd_match_indices:
-        # If it was pruned by mean chroma/length pruning, compute manually
-        if msd_index not in matches:
-            _, score, _ = hash_match.match_one_sequence(
-                midi_data['hash_list'], [sequences[msd_index]], GULLY, PENALTY)
-            score = score[0]
-            rank = (scores < score).sum()
-        # Otherwise find the result
-        else:
-            rank = matches.index(msd_index)
-            score = scores[rank]
-        matched_ranks.append(rank)
-        matched_scores.append(score)
-    results['msd_match_ranks'] = matched_ranks
-    results['msd_match_scores'] = matched_scores
+    results['msd_match_ranks'] = [
+        matches.index(msd_index) for msd_index in msd_match_indices]
+    results['msd_match_scores'] = [
+        scores[rank] for rank in results['msd_match_ranks']]
     results['n_pruned_dist'] = n_pruned_dist
-
     return results
 
-midi_datas = []
-msd_match_index_lists = []
-midi_h5_mapping = []
-with open(PAIR_FILE) as f:
-    for line in f.readlines():
-        midi_md5, dataset, msd_id = line.strip().split(',')
-        # The pairs.csv files will include pairs from all datasets
-        # Only grab those for the MSD
-        if dataset == 'msd':
-            # Only include if the alignment was successful
-            alignment_file = os.path.join(
-                ALIGNMENT_DIAGNOSTICS_PATH,
-                'msd_{}_{}.npz'.format(msd_id, midi_md5))
-            if os.path.exists(alignment_file):
-                diagnostics = np.load(alignment_file)
-                if diagnostics['score'] < SCORE_THRESHOLD:
-                    midi_h5_mapping.append([midi_md5, msd_id])
+if __name__ == '__main__':
+    # Load in list of MSD entries
+    msd_index = whoosh_search.get_whoosh_index(
+        os.path.join(DATA_PATH, 'msd', 'index'))
+    with msd_index.searcher() as searcher:
+        msd_list = list(searcher.documents())
 
-for midi_md5, msd_id in midi_h5_mapping:
-    midi_entry = [entry for entry in midi_index if entry['md5'] == midi_md5]
-    if len(midi_entry) == 0:
-        continue
-    else:
-        midi_entry = midi_entry[0]
-    midi_pkl = os.path.join('../data/clean_midi/pkl/',
-                            midi_entry['path'] + '.pkl')
-    try:
-        with open(midi_pkl) as f:
-            midi_data = pickle.load(f)
-    except:
-        continue
-    if any([(midi_data['md5'] == d['md5']) for d in midi_datas]):
-        continue
-    if msd_id not in msd_ids:
-        continue
-    midi_datas.append(midi_data)
-    msd_match_ids = [id for md5, id in midi_h5_mapping
-                     if md5 == midi_data['md5']]
-    msd_match_indices = []
-    for id in msd_match_ids:
-        if id in msd_ids:
-            msd_match_indices.append(msd_ids.index(id))
-    msd_match_index_lists.append(msd_match_indices)
+    # Load in list of MSD entries
+    midi_index = whoosh_search.get_whoosh_index(
+        os.path.join(DATA_PATH, 'clean_midi', 'index'))
+    with midi_index.searcher() as searcher:
+        midi_list = list(searcher.documents())
 
-results = joblib.Parallel(n_jobs=11, verbose=51)(
-    joblib.delayed(match_one_midi)(midi_data, msd_match_indices)
-    for midi_data, msd_match_indices in zip(midi_datas, msd_match_index_lists))
+    # Load in hash sequences (and metadata) for all MSD entries
+    msd_data = []
+    for entry in msd_list:
+        mpk_file = os.path.join(
+            RESULTS_PATH, 'dhs_msd_hash_sequences', entry['path'] + '.mpk')
+        # If creating a CQT or hashing failed, there will be no file
+        if os.path.exists(mpk_file):
+            try:
+                with open(mpk_file) as f:
+                    data = msgpack.unpackb(f.read())
+                # Ignore very short sequences
+                if len(data['hash_sequence']) < MIN_SEQUENCE_LENGTH:
+                    continue
+                msd_data.append(data)
+            except Exception as e:
+                print "Error loading {}: {}".format(mpk_file, e)
+    # Create a separate list of the IDs of each entry in msd_data
+    msd_data_ids = [d['id'] for d in msd_data]
 
-with open(RESULTS_FILE, 'wb') as f:
-    json.dump(results, f)
+    # Get a list of valid MIDI-MSD match pairs
+    midi_msd_mapping = utils.get_valid_matches(
+        os.path.join(RESULTS_PATH, '{}_pairs.csv'.format(SPLIT)),
+        SCORE_THRESHOLD,
+        os.path.join(RESULTS_PATH, 'clean_midi_aligned', 'h5'))
+
+    # We will create a new dict which only contains indices of correct matches
+    # in the msd_sequences list, and only for matches we could load in
+    midi_index_mapping = {}
+    # Also create dict of loaded MIDI data
+    midi_datas = {}
+    for midi_md5, msd_ids in midi_msd_mapping.items():
+        midi_entry = [entry for entry in midi_list if entry['id'] == midi_md5]
+        # Edge case - no entry in the MIDI list for this md5
+        if len(midi_entry) == 0:
+            continue
+        else:
+            midi_entry = midi_entry[0]
+        # Load in data (hash sequence and metadata) for this MIDI file
+        midi_mpk = os.path.join(RESULTS_PATH, 'dhs_clean_midi_hash_sequences',
+                                midi_entry['path'] + '.mpk')
+        try:
+            with open(midi_mpk) as f:
+                midi_data = msgpack.unpackb(f.read())
+        except Exception as e:
+            continue
+        # We were successful, so save this data and matching indices
+        midi_datas[midi_md5] = midi_data
+        midi_index_mapping[midi_md5] = [msd_data_ids.index(i) for i in msd_ids]
+
+    # Run match_one_midi for each MIDI data and MSD index list
+    results = joblib.Parallel(n_jobs=11, verbose=51)(
+        joblib.delayed(match_one_midi)(
+            midi_datas[md5], msd_data, midi_index_mapping[md5])
+        for md5 in midi_datas)
+
+    # Create DHS match results output path if it doesn't exist
+    output_path = os.path.join(RESULTS_PATH, 'dhs_match_results')
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    # Save list of all matching results
+    results_file = os.path.join(output_path, '{}_results.h5'.format(SPLIT))
+    deepdish.io.save(results_file, results)
