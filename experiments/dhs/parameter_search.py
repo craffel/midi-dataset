3f58bd05d5aa0c1458702ce7351886f5b36c5ff4
Colin Raffel <craffel@gmail.com>
2016-03-07 23:30:12 -0500

    Increase negative_importance range in dhs
    This reflects the changes in
    https://github.com/craffel/dhs/commit/4664fbcbfd0e3f98b67c9c4c925766d4f5ebe64f
    https://github.com/craffel/dhs/commit/db94b4e99759ecee9da1bfa259ca4a44d310639f
    These two changes mean that the cost terms other than negative
    importance are now n_bits times as high as they were.  So, we should
    increase the range to [0, n_bits] instead of [0, 1].  But, in practice,
    I didn't see any settings for this hyperparameter over .5, so setting it
    to 20 seems fine.

--- experiments/dhs/parameter_search.py
+++ experiments/dhs/parameter_search.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         'negative_threshold': {'type': 'int', 'min': 1, 'max': 16},
         'dropout': {'type': 'int', 'min': 0, 'max': 1},
         'learning_rate': {'type': 'float', 'min': .0001, 'max': .01},
-        'negative_importance': {'type': 'float', 'min': 0.01, 'max': 1.},
+        'negative_importance': {'type': 'float', 'min': 0.01, 'max': 20.},
         'entropy_importance': {'type': 'float', 'min': 0.0, 'max': 1.},
         'downsample_frequency': {'type': 'int', 'min': 0, 'max': 1},
         'network': {'type': 'enum',
